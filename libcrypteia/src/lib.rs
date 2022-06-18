4c7cd27fbc2259cfb45c438b188a1227ab5a4ec5
Ken Collins <ken@metaskills.net>
2022-06-18 15:31:42 +0000

    Testing libcrypteia

--- libcrypteia/src/lib.rs
+++ libcrypteia/src/lib.rs
@@ -5,7 +5,7 @@ use std::ffi::CString;
 
 redhook::hook! {
     unsafe fn getenv(name: *const libc::c_char) -> *mut libc::c_char => dangerously_mutate_getenv {
-      println!("Hello redhook!");
+      // println!("Hello redhook!");
       let given_pointer = redhook::real!(getenv)(name);
       if given_pointer.is_null() {
           return given_pointer;
@@ -13,7 +13,7 @@ redhook::hook! {
 
       let given_name = CStr::from_ptr(name).to_str().unwrap();
       let given_value = CStr::from_ptr(given_pointer).to_str().unwrap();
-      println!("Got: {} / {}", given_name, given_value);
+      println!("Got: {}:{}", given_name, given_value);
 
       if given_name == "HELLO" {
           // https://doc.rust-lang.org/std/ffi/struct.CStr.html#method.as_ptr
