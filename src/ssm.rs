cab70ce045c01fbbeaf65b68a1edc317277ebfd3
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2024-06-18 11:42:42 -0400

    update aws config and ssm

--- src/ssm.rs
+++ src/ssm.rs
@@ -127,7 +127,7 @@ async fn ssm_get_parameters_by_path(
 mod test {
     use super::*;
     use anyhow::Result;
-    use aws_sdk_ssm::model::ParameterType;
+    use aws_sdk_ssm::types::ParameterType; // Corrected import
     use std::collections::HashMap;
 
     #[tokio::test]
